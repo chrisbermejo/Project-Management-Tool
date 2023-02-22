a41c60aef12d61574ce2f75e7e749a07108dd921
chrisbermejo <chrisbermejo1@gmail.com>
2023-02-22 05:23:40 -0500

    ?

--- routes/members.js
+++ routes/members.js
@@ -15,11 +15,23 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/api/:memberId/edit', async (req, res) => {
+router.get('/api/:memberID/edit', async (req, res) => {
   try {
-    const memberId = req.params.memberId;
-    const memberINFO = await member.findOne({memberid: memberId});
+    const memberID = req.params.memberID;
+    console.log(memberID);
+    const memberINFO = await member.findOne({_id: memberID});
     console.log(memberINFO);
+    res.json(memberINFO);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+
+router.get('/view/:memberID', async (req, res) => {
+  try {
+    const memberID = req.params.memberID;
+    const memberINFO = await member.findOne({_id: memberID});
     res.json(memberINFO);
   } catch (err) {
     console.error(err);
