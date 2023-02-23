a5b69b544d5d6cf5fe8da62cd6b395890b63c8eb
chrisbermejo <chrisbermejo1@gmail.com>
2023-02-22 23:36:39 -0500

    test

--- src/js/script.js
+++ src/js/script.js
@@ -2,68 +2,68 @@
 function validation() {
   let isValid = true;
 
-  if (memberid.value === "" || isNaN(memberid.value) || parseInt(memberid.value) < 1) {
-    memberidError.classList.add('label-error');
-    memberid.classList.add('input-error');
-    isValid = false;
-  } else {
-    memberidError.classList.remove('label-error');
-    memberid.classList.remove('input-error');
-  }
+  // if (memberid.value === "" || isNaN(memberid.value) || parseInt(memberid.value) < 1) {
+  //   memberidError.classList.add('label-error');
+  //   memberid.classList.add('input-error');
+  //   isValid = false;
+  // } else {
+  //   memberidError.classList.remove('label-error');
+  //   memberid.classList.remove('input-error');
+  // }
 
-  if (fname.value === "" || !/^[a-zA-Z ]+$/.test(fname.value)) {
-    fnameError.classList.add('label-error');
-    fname.classList.add('input-error');
-    isValid = false;
-  } else {
-    fnameError.classList.remove('label-error');
-    fname.classList.remove('input-error');
-  }
+  // if (fname.value === "" || !/^[a-zA-Z ]+$/.test(fname.value)) {
+  //   fnameError.classList.add('label-error');
+  //   fname.classList.add('input-error');
+  //   isValid = false;
+  // } else {
+  //   fnameError.classList.remove('label-error');
+  //   fname.classList.remove('input-error');
+  // }
 
-  if (lname.value === "" || !/^[a-zA-Z ]+$/.test(lname.value)) {
-    lnameError.classList.add('label-error');
-    lname.classList.add('input-error');
-    isValid = false;
-  } else {
-    lnameError.classList.remove('label-error');
-    lname.classList.remove('input-error');
-  }
+  // if (lname.value === "" || !/^[a-zA-Z ]+$/.test(lname.value)) {
+  //   lnameError.classList.add('label-error');
+  //   lname.classList.add('input-error');
+  //   isValid = false;
+  // } else {
+  //   lnameError.classList.remove('label-error');
+  //   lname.classList.remove('input-error');
+  // }
 
-  if (email.value === "" || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) {
-    emailError.classList.add('label-error');
-    email.classList.add('input-error');
-    isValid = false;
-  } else {
-    emailError.classList.remove('label-error');
-    email.classList.remove('input-error');
-  }
+  // if (email.value === "" || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) {
+  //   emailError.classList.add('label-error');
+  //   email.classList.add('input-error');
+  //   isValid = false;
+  // } else {
+  //   emailError.classList.remove('label-error');
+  //   email.classList.remove('input-error');
+  // }
 
-  if (role.value === "") {
-    roleError.classList.add('label-error');
-    role.classList.add('input-error');
-    isValid = false;
-  } else {
-    roleError.classList.remove('label-error');
-    role.classList.remove('input-error');
-  }
+  // if (role.value === "") {
+  //   roleError.classList.add('label-error');
+  //   role.classList.add('input-error');
+  //   isValid = false;
+  // } else {
+  //   roleError.classList.remove('label-error');
+  //   role.classList.remove('input-error');
+  // }
 
-  if (isActive.value === "") {
-    isActiveError.classList.add('label-error');
-    isActive.classList.add('input-error');
-    isValid = false;
-  } else {
-    isActiveError.classList.remove('label-error');
-    isActive.classList.remove('input-error');
-  }
+  // if (isActive.value === "") {
+  //   isActiveError.classList.add('label-error');
+  //   isActive.classList.add('input-error');
+  //   isValid = false;
+  // } else {
+  //   isActiveError.classList.remove('label-error');
+  //   isActive.classList.remove('input-error');
+  // }
 
-  if (teamid.value === "" || isNaN(teamid.value) || parseInt(teamid.value) < 1) {
-    teamidError.classList.add('label-error');
-    teamid.classList.add('input-error');
-    isValid = false;
-  } else {
-    teamidError.classList.remove('label-error');
-    teamid.classList.remove('input-error');
-  }
+  // if (teamid.value === "" || isNaN(teamid.value) || parseInt(teamid.value) < 1) {
+  //   teamidError.classList.add('label-error');
+  //   teamid.classList.add('input-error');
+  //   isValid = false;
+  // } else {
+  //   teamidError.classList.remove('label-error');
+  //   teamid.classList.remove('input-error');
+  // }
 
   return isValid;
 }
@@ -130,7 +130,7 @@ function validateForm(event) {
 
   if (isValid) {
     closeAddMemberDialog[0].addEventListener('click', function() {
-      demoDialog.close();
+      AddMemberDialog.close();
     });      
     event.target.submit();
   }
@@ -154,6 +154,7 @@ let edit_teamid = document.getElementById("edit-teamid");
 let edit_button = document.getElementById("edit-Update");
 
 //Edit Member Dialog
+let edit_memberform = document.getElementById('edit-member-form');
 let openEditMemberDialogButton = document.querySelectorAll('.openEditMemberButton');
 let EditMemberDialog = document.getElementById('editMemberForm');
 let closeEditMemberDialog = document.querySelectorAll('.closeEditMember');
@@ -172,6 +173,7 @@ openEditMemberDialogButton.forEach(editButton => {
       const response = await fetch(`/dashboard/members/api/${memberId}`);
       const member = await response.json();
       console.log(member._id);
+      edit_memberform.action = `/dashboard/members/member/${member._id}/edit/update`
       edit_memberid.value = member.memberid;
       edit_fname.value = member.fname;
       edit_lname.value = member.lname;
@@ -179,10 +181,9 @@ openEditMemberDialogButton.forEach(editButton => {
       edit_role.value = member.role;
       edit_isActive.value = member.isActive;
       edit_teamid.value = member.teamid;
-      edit_button.value = member._id;
 
       EditMemberDialog.showModal();
-      const url = `/dashboard/members/edit/${member.memberid}`;
+      const url = `/dashboard/members/member/${member.memberid}/edit`;
 			const title = "EDIT";
 			history.pushState(null, title, url);
 
@@ -192,3 +193,15 @@ openEditMemberDialogButton.forEach(editButton => {
     }
   });
 });
+
+function edit_validateForm(event) {
+  event.preventDefault();
+  let isValid = validation();
+
+  if (isValid) {
+    closeEditMemberDialog[0].addEventListener('click', function() {
+      EditMemberDialog.close();
+    });      
+    event.target.submit();
+  }
+}
